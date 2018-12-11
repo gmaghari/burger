3d124a2bb6ff628495a68dcd4b118c027b890dab
Garry Maghari <garry.maghari@gmail.com>
2018-12-11 12:31:57 -0800

    Update server.js and main.handlebars

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3560;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());

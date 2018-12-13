22b7daaebca019dfbcd1af78101c93ae3ba8251d
Garry Maghari <garry.maghari@gmail.com>
2018-12-12 17:37:54 -0800

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -3,23 +3,23 @@ var orm = require('../config/orm.js');
 
 
 // Input burger info for ORM
+var orm = require("../config/orm.js");
+
 var burger = {
-    selectAll: function(callback){
-        orm.selectAll(function(res){
-        callback(res);
-        });
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
     },
-
-    insertOne: function(burger_name, callback){
-        orm.insertOne(burger_name, function(res){
-        callback(res);
-        });
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
     },
-
-    updateOne: function(burger_id, callback){
-        orm.updateOne(burger_id, function(res){
-        callback(res);
-        });
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
     }
 };
 

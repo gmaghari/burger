6c667d0a078ec27d3c10f1a06786101d0dd6a751
Garry Maghari <garry.maghari@gmail.com>
2018-12-12 22:51:25 -0800

    Update controller

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,19 @@
-// Import the ORM to create functions to interface with db
-var orm = require('../config/orm.js');
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
 var burger = {
   selectAll: function(cb) {
-    orm.selectAll('burgers', function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
-
   insertOne: function(cols, vals, cb) {
-    orm.insertOne('burgers', cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
-  updateOne: function(obj, condition, cb) {
-    orm.updateOne('burgers', obj, condition, function(res) {
-
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'));
-
+app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require('express-handlebars');
+app.use(methodOverride("_method"));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+var exphbs = require("express-handlebars");
 
-var routes = require('./controllers/burgers_controller.js');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.use(routes);
+// Import routes
+var routes = require("./controllers/burgers_controller.js");
 
-app.listen(PORT, function() {
-  console.log('App now listening at localhost:' + PORT);
+app.use("/", routes);
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
 });
